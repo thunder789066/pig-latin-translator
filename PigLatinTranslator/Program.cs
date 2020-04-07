5a01129da679598262d8bac2efa24881ecd12e41
Christina Gerstner <christinalgerstner@gmail.com>
2020-04-07 18:35:35 -0500

    Initial commit

---
+++ PigLatinTranslator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatinTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a sentence: \t");
+            string sentence = Console.ReadLine();
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
